264cd2fb0df95988b4c7832acf0a5bea8161524d
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2020-07-22 18:21:21 -0300

    adding beta UI

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,10 @@ const request =
 void main() async {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+      hintColor: Colors.amber,
+      primaryColor: Colors.white
+    ),
   ));
 }
 
@@ -18,6 +22,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  double dolar;
+  double euro;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,8 +56,45 @@ class _HomeState extends State<Home> {
                     textAlign: TextAlign.center,
                   ));
                 } else {
-                  return Container(
-                    color: Colors.green,
+                  dolar = snapshot.data["results"] ["currencies"] ["USD"] ["buy"];
+                  euro = snapshot.data["results"] ["currencies"] ["EUR"] ["buy"];
+                  return SingleChildScrollView(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on, size: 150, color: Colors.amber),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: "Reais",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefixText: "R\$"
+                          ),
+                          style: TextStyle(color: Colors.amber, fontSize: 25),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Dólares",
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: "US\$"
+                          ),
+                          style: TextStyle(color: Colors.amber, fontSize: 25),
+                        ),
+                        Divider(),
+                        TextField(
+                          decoration: InputDecoration(
+                              labelText: "Euros",
+                              labelStyle: TextStyle(color: Colors.amber),
+                              border: OutlineInputBorder(),
+                              prefixText: "€"
+                          ),
+                          style: TextStyle(color: Colors.amber, fontSize: 25),
+                        )
+                      ],
+                    ),
                   );
                 }
             }
